6157b29bab33317389d56b9097d7e392353701fe
Yannitamba <hamedrachid760@gmail.com>
2024-07-06 13:59:27 +0100

    Yann commit 1

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,4 @@
-from  .views import (index, table, staticLayout,
-                      charts, login, register, erro401,
-                        erro404, erro500, changerPassword)
+from  .views import *
 from django.urls import path
 
 urlpatterns = [
@@ -10,6 +8,10 @@ urlpatterns = [
     path('charts', charts, name='charts'),
     path('login', login, name='login'),
     path('register', register, name='register'),
+    path('register-debit', register_debit, name='register-debit'),
+    path('utilisateurs', utilisateurs, name='utilisateurs'),
+    path('historique', historique, name='historique'),
+    path('stocks', stocks, name='stocks'),
     path('error401', erro401, name='error401'),
     path('error404', erro404, name='error404'),
     path('error500', erro500, name='error500'),
--- index/views.py
+++ index/views.py
@@ -3,37 +3,42 @@ from django.shortcuts import render
 # Create your views here.
 
 def index (request):
-
     return render(request, 'index/index.html')
 
 def table (request):
-
     return render(request, 'index/tables.html')
 
 def staticLayout(request):
-
     return render(request, 'index/layout-static.html')
 
 def charts(request):
-
     return render(request, 'index/charts.html')
 
 def login (request):
-
     return render(request, 'index/login.html')
 
 def register (request):
     return render (request, 'index/register.html')
 
+def register_debit (request):
+    return render (request, 'index/register-debit.html')
+
+def utilisateurs (request):
+    return render (request, 'index/utilisateurs.html')
+
+def historique (request):
+    return render (request, 'index/historique.html')
+
+def stocks (request):
+    return render (request, 'index/stocks.html')
+
 def changerPassword (request):
     return render (request, 'index/password.html')
 
 def erro401(request):
-
     return render(request, 'index/401.html')
 
 def erro404 (request):
-
     return render(request, 'index/404.html')
 
 def erro500 (request):
